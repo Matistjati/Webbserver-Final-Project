219794733b8ae4be221e17938aa09bd172ccae95
Matistjati <matistjati@outlook.com>
2022-04-20 16:41:41 +0200

    You can now select a table and view its content in debug_view

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
-#require_relative 'model.rb'
+require_relative 'common.rb'
 
 enable :sessions
 
@@ -10,5 +10,31 @@ enable :sessions
 
 
 get("/") do
-    
+    puts("ligma")
+    slim(:index)
+end
+
+get("/debug_view") do
+    # Get all tables
+    selected_table = session[:debug_table_selected]
+
+    db = connect_to_db("database")
+
+    table = nil
+    if selected_table != nil
+        # If table is selected, get its data
+        table = db.execute("SELECT * FROM #{selected_table}")
+    end
+
+    # Get names of tables
+    tables = db.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+
+    slim(:debug, locals:{"tables":tables, "selected": selected_table, "viewed_table": table})
+end
+
+post("/select_db") do
+    session[:debug_table_selected] = params[:selected_db]
+
+    redirect("/debug_view")
 end
---
+++ common.rb
@@ -0,0 +1,5 @@
+def connect_to_db(name, rootDir="db")
+    db = SQLite3::Database.new("#{rootDir}/#{name}.db")
+    db.results_as_hash = true
+    return db
+end
