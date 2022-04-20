d8f3f6de6205306d7c408b4bcc4f10b3b19ff645
Matistjati <matistjati@outlook.com>
2022-04-20 17:17:05 +0200

    You can now delete and add rows in the debug view

--- app.rb
+++ app.rb
@@ -14,8 +14,9 @@ get("/") do
     slim(:index)
 end
 
-get("/debug_view") do
+get("/debug") do
     # Get all tables
+    #session[:debug_table_selected] = "users"
     selected_table = session[:debug_table_selected]
 
     db = connect_to_db("database")
@@ -33,8 +34,38 @@ get("/debug_view") do
     slim(:debug, locals:{"tables":tables, "selected": selected_table, "viewed_table": table})
 end
 
+
 post("/select_db") do
     session[:debug_table_selected] = params[:selected_db]
 
-    redirect("/debug_view")
+    redirect("/debug")
+end
+
+post("/add_row") do
+    value = "("
+    for field in params do
+        value += "\"" + field[1] + "\""
+        value += ","
+    end
+
+    value = value[0...-1]
+
+    value += ")"
+
+    db = connect_to_db("database")
+    puts("INSERT INTO #{session[:debug_table_selected]} VALUES #{value}")
+    db.execute("INSERT INTO #{session[:debug_table_selected]} VALUES #{value}")
+
+
+    redirect("/debug")
+end
+
+post("/delete_row") do 
+    row = params[:row].to_i
+
+    db = connect_to_db("database")
+    
+    db.execute("DELETE FROM #{session[:debug_table_selected]} WHERE id in (SELECT id FROM #{session[:debug_table_selected]} LIMIT 1 OFFSET #{row})")
+
+    redirect("/debug")
 end
